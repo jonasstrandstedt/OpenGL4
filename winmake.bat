35dac2c2ff56b2c6c1167da7de4ee9c8a9854e7b
jonasstrandstedt <jonas.strandstedt@gmail.com>
2013-06-08 16:03:52 +0200

    Fixed bug

--- winmake.bat
+++ winmake.bat
@@ -1,10 +1,6 @@
 @ECHO off
 set PATH=%PATH%;C:\MinGW\bin
 set PATH=%PATH%;C:\MinGW\msys\1.0\bin
-pushd %~dp0
 mingw32-make -f Makefile clean
 mingw32-make -f Makefile
-IF NOT EXIST C:\Windows\System32\glew32.dll (
-	xcopy GL4-engine\lib\mingw32\glew32.dll C:\Windows\System32\
-)
 PAUSE
